42f05fb8962cb9fa9547c2d7181331d6a31307dd
Gareth Bk <garethbk@gmail.com>
2019-09-09 14:05:31 -0700

    Set up sass vars and base styles, organize directory structure, create header component

---
+++ src/components/app/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.scss';
+
+// import components
+import Header from '../header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.scss';
+
+function Header() {
+  return (
+    <header className="header-wrapper">
+      <div className="container header-content">
+        <h1>Awesome Axes</h1>
+        <p>The #1 resource for finding your next sick shredder to make heads explode and achieve rock god status</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
